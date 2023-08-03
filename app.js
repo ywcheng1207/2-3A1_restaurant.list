f7b8d23c66f0aa85fc36d2a232da61c0a01d1da9
ywcheng1207 <ywcheng1207.git@gmail.com>
2023-08-04 00:52:15 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,42 +1,62 @@
 // app.js
-// require packages used in the project
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+// ----------------------------------------- mongo
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+// -----------------------------------------
+
 
 // require handlebars in the project
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars')
 
 //data
-const restaurantList = require("./restaurant.json");
+const restaurantList = require('./restaurants.json')
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // setting static files
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 // routes setting
-app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
-});
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
 
-app.get("/search", (req, res) => {
-  const keyword = req.query.keyword;
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter((restaurant) => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
-  });
-  res.render("index", { restaurants: restaurants, keyword: keyword });
-});
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
 
-app.get("/restaurants/:restaurant_id", (req, res) => {
+app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(
     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
-  );
-  res.render("show", { restaurant: restaurant });
-});
+  )
+  res.render('show', { restaurant: restaurant })
+})
 
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`);
-});
+  console.log(`Express is listening on http://localhost:${port}`)
+})
